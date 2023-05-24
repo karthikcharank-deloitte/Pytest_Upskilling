b36eb2cc7e74c81ddda40e4778d53c2ed55354ab
karthikck <karthikck@deloitte.com>
2023-05-24 10:55:28 +0530

    Mini Assignment 3 updated with Cross-browser support

--- Mini_assignment_2/conftest.py
+++ Mini_assignment_2/conftest.py
@@ -1,25 +1,28 @@
 import pytest
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
 
 driver = None
+input1 = ("2023-06-06", "12:00", "2023-06-07", "12:00", "chrome")
+input2 = ("2023-06-14", "09:00", "2023-06-14", "11:00", "firefox")
 
 
 @pytest.fixture(scope="class")
-def setup(request):
+def setup(request, test_data):
     global driver
-    driver = webdriver.Chrome(ChromeDriverManager().install())
+    if test_data[4] == "chrome":
+        driver = webdriver.Chrome(ChromeDriverManager().install())
+    elif test_data[4] == "firefox":
+        driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
     driver.maximize_window()
-    driver.implicitly_wait(5)
+    driver.implicitly_wait(10)
     request.cls.driver = driver
     yield
     driver.close()
 
 
-input1 = ("2023-06-06", "12:00", "2023-06-07", "12:00")
-input2 = ("2023-06-14", "09:00", "2023-06-14", "11:00")
-
-
-@pytest.fixture(params=[input1, input2])
+@pytest.fixture(params=[input1, input2], scope="module")
 def test_data(request):
     return request.param
